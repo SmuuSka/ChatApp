2b43f2b36ac934a89f4f9bc15a82ba51a15cf245
KasparT <kaspar.tullusgmail.com>
2023-04-13 22:18:23 +0300

    Navbar,Home and Components added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar= () =>{
   return(
       <nav className="chatNavbar">
-          <img id ="chatUserImg" src="client/src/pages" alt=""/>
+          <img id ="chatUserImg" src="https://cdn.pixabay.com/photo/2016/12/21/00/36/woman-1921883_960_720.jpg" alt=""/>
           <div className="userNavbar">
         <span id = "chatUserName">Jussi</span>
         <button className="buttonLogout">Logout</button>
