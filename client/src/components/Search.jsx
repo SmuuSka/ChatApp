21d305003c95f8be0055c6565702bbb829b28327
KasparT <kaspar.tullusgmail.com>
2023-04-13 22:18:23 +0300

    Navbar,Home and Components added

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 const Search = () => {
     return(
-        <>
-        </>
+        <div className="searchBar">
+            <form className="searchForm">
+                <h3 id="searchbarTitle">Search for Users</h3>
+                <input className="searchInput" type="text"  placeholder="Find a User"/>
+            </form>
+            <div className="searchFriendChatInfo">
+            <img id="searchFriendImage" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt=""/>
+                <span id="searchUsername">Jane</span>
+            </div>
+        </div>
     );
 }
 export default Search;
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./Navbar";
+import Search from "./Search";
+import Chats from "./Chats";
 
 const SideBar= () =>{
     return(
         <div className="sideBarChat">
             <Navbar/>
+            <Chats/>
+            <Search/>
+
         </div>
     );
 }
