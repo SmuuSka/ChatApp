46c219fb4f0474bf768a3b568d74a325c5c748a5
KasparT <kaspar.tullusgmail.com>
2023-04-14 14:30:27 +0300

    Chat,Services added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,33 @@
 import BGVIDEO from "../cssFiles/backgroundvid.mp4"
+import {useState} from "react";
+
+import loginService from '../services/loginService';
 const Login = () =>{
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const usernameChange = (event) => {
+    setUsername(event.target.value)
+    console.log(username)
+  }
+
+  const passwordChange = (event) => {
+    setPassword(event.target.value)
+    console.log(password)
+  }
+
+  const onLogin = async (event) => {
+    event.preventDefault()
+    const credentials = {username: username, password: password}
+    try {
+      console.log(credentials)
+      const response = await loginService.login(credentials)
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return(
     <>
       <div id = "loginBody" className="formContainerReg">
@@ -13,13 +41,11 @@ const Login = () =>{
           <img className="RegLogoimg" src="Logochat.png"/>
         </span>
           </div>
-          <form className="form-group">
-            <input id = "loginUsername"  type="text" className="form-control" placeholder="Username/Display Name"/>
-            <input id = "loginPassWord" type="password"  className="form-control" placeholder="Password"/>
+          <form className="form-group" onSubmit={onLogin}>
+            <input id = "loginUsername"  type="text" className="form-control" placeholder="Username/Display Name" onChange={usernameChange}/>
+            <input id = "loginPassWord" type="password"  className="form-control" placeholder="Password" onChange={passwordChange}/>
             <button  id = "buttonLogReg" type="button" className="btn btn-primary">Login</button>
-            <p>Dont have an account?
-
-            </p>
+            <p>Dont have an account? Register</p>
           </form>
 
         </div>
