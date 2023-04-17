8f5b12ee541dadc4378adfa964f8d3b84af17e44
SmuuSka <samuaikio@gmail.com>
2023-04-17 15:00:39 +0300

    Tietokannan testaus

--- server/database.js
+++ server/database.js
@@ -3,6 +3,29 @@ const config = require('./utils/config');
 
 const pool = mariadb.createPool(config.sql);
 
+// eslint-disable-next-line require-jsdoc
+// async function createUserTable() {
+//   let conn;
+//   try {
+//     conn = await pool.getConnection();
+//     console.log('connection succeeded');
+//     await conn.query(
+//         `CREATE TABLE IF NOT EXISTS testi (
+//           id int AUTO_INCREMENT PRIMARY KEY,
+//           username varchar(50) NOT NULL,
+//           password varchar(200) NOT NULL
+//         )`,
+//     );
+//     console.log('query succeeded');
+//   } catch (err) {
+//     console.log(err);
+//     throw err;
+//   } finally {
+//     if (conn) return conn.end();
+//   }
+// }
+
+//createUserTable();
 
 module.exports = pool;
 
